26bfeee6075a57e1cb2dbe619b2e199ee72bac46
Manishakoirala G <manishakoiralabca17@gmail.com>
2022-10-05 17:09:36 +0530

    chnages

--- ChatAppNode/index.js
+++ ChatAppNode/index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require("socket.io", {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-  res.sendFile("/index.html", { root: __dirname });
-});
+// app.get("/", (req, res) => {
+//   console.log("server");
+// });
 io.on("connection", (socket) => {
   console.log("connect");
   io.emit("connection message", "some user connected");
